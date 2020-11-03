d484cb7bd1f6ecc5b33f6a8b2853cf006e11edff
ZS <zh.sundetov@kazdream.kz>
2020-11-03 14:52:54 +0300

    2. tutorial

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,12 @@
 All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 
+## [1.1.0] - change(basic-lessons) - 2020-11-03
+
+### Learned
+
+- v-bind, v-html, methods
+
 ## [1.0.0] - feat(basic-lessons) - 2020-11-03
 
 ### Added
--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-Vue.createApp({
-  data() {
-    return {
-      goals: [],
-      enteredValue: "",
-    };
-  },
-  methods: {
-    addGoal() {
-      this.goals.push(this.enteredValue);
-      this.enteredValue = "";
-    },
-  },
-}).mount("#app");
+// 2.const app = Vue.createApp({
+//   data() {
+//     return {
+//       courseGoal: 'Finish the course and learn Vue!',
+//       vueLink: 'https://vuejs.org/',
+//     }
+//   },
+//   methods: {
+//     outputGoal() {
+//       return (string = 'My string created by method')
+//     },
+//   },
+// })
+// app.mount('#user-goal')
+
+// 1.Vue.createApp({
+//   data() {
+//     return {
+//       goals: [],
+//       enteredValue: "",
+//     };
+//   },
+//   methods: {
+//     addGoal() {
+//       this.goals.push(this.enteredValue);
+//       this.enteredValue = "";
+//     },
+//   },
+// }).mount("#app");
 
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
